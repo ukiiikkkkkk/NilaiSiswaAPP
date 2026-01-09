d184feb8f7885d388ea87f18b7b75a769093d3d7
ukiiikkkkkk <azry_syauqi_34pg@student.smktelkom-mlg.sch.id>
2026-01-09 15:26:45 +0700

    Menambah Input Nama Dan Nilai

--- Program.cs
+++ Program.cs
@@ -2,3 +2,10 @@
 string nama;
 
 Console.WriteLine("aplikasi nilai siswa");
+
+Console.WriteLine("Nama Siswa: ");
+nama = Console.ReadLine();
+
+Console.Write("Nilai: ");
+nilai = int.Parse(Console.ReadLine());
+
